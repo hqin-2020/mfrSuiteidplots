a96bd833c7b259c0600502e37b9a42fc24e51f5f
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-16 20:33:16 -0600

    1

--- SolvedModels/run_MFR_4d.sh
+++ SolvedModels/run_MFR_4d.sh
@@ -23,7 +23,7 @@ do
     do
         for a_h in -1
         do
-            for gamma_e in 2.0 3.0 4.0
+            for gamma_e in 4.0
             do
                 for gamma_h in 6.0 8.0
                 do
@@ -35,14 +35,14 @@ do
                             mkdir -p ./job-outs/$domain_folder/$model_folder
                             mkdir -p ./bash/$domain_folder/$model_folder
 
-                            touch ./bash/$domain_folder/$model_folder/run.sh
-                            tee ./bash/$domain_folder/$model_folder/run.sh << EOF
+                            touch ./bash/$domain_folder/$model_folder/run4d.sh
+                            tee ./bash/$domain_folder/$model_folder/run4d.sh << EOF
 #! /bin/bash
 
 #SBATCH --account=pi-lhansen
-#SBATCH --job-name=run
-#SBATCH --output=./job-outs/$domain_folder/$model_folder/run.out
-#SBATCH --error=./job-outs/$domain_folder/$model_folder/run.err
+#SBATCH --job-name=run4d
+#SBATCH --output=./job-outs/$domain_folder/$model_folder/run4d.out
+#SBATCH --error=./job-outs/$domain_folder/$model_folder/run4d.err
 #SBATCH --time=0-5:00:00
 #SBATCH --partition=caslake
 #SBATCH --nodes=1
@@ -55,7 +55,7 @@ python3 /project/lhansen/mfrSuiteidplots/SolvedModels/run_mfrSuite_4d.py --chiUn
                                                     --nV ${nV} --nVtilde ${nVtilde} --V_bar ${V_bar} --Vtilde_bar ${Vtilde_bar} --sigma_V_norm ${sigma_V_norm} --sigma_Vtilde_norm ${sigma_Vtilde_norm} \
 
 EOF
-                            sbatch ./bash/$domain_folder/$model_folder/run.sh
+                            sbatch ./bash/$domain_folder/$model_folder/run4d.sh
                         done
                     done
                 done
