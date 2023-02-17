6bef553b1c982c5e38407b0d1105f9fcee7172de
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-16 20:34:36 -0600

    1

--- SolvedModels/run_MFR_quarter.sh
+++ SolvedModels/run_MFR_quarter.sh
@@ -23,7 +23,7 @@ do
     do
         for a_h in -1
         do
-            for gamma_e in 2.0 3.0 4.0
+            for gamma_e in 4.0
             do
                 for gamma_h in 6.0 8.0
                 do
