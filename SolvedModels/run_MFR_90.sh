a8704772ef6f7d66658e8cbbedaf877fca99d5d6
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-17 23:12:17 -0600

    1

--- SolvedModels/run_MFR_90.sh
+++ SolvedModels/run_MFR_90.sh
@@ -46,8 +46,8 @@ do
 #SBATCH --time=0-12:00:00
 #SBATCH --partition=caslake
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=14
-#SBATCH --mem-per-cpu=4000
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=3000
 
 module load python/anaconda-2021.05
 
