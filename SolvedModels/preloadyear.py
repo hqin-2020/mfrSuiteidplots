54804d83054c71d52f946811659e52b37e2cee04
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-19 00:43:14 -0600

    1

--- SolvedModels/preloadyear.py
+++ SolvedModels/preloadyear.py
@@ -121,7 +121,7 @@ chiUnderline = str("{:0.3f}".format(params['chiUnderline'])).replace('.', '', 1)
 folder_name = 'chiUnderline_' + chiUnderline + '_a_e_' + a_e + '_a_h_' + a_h  + '_gamma_e_' + gamma_e + '_gamma_h_' + gamma_h + '_psi_e_' + psi_e + '_psi_h_' + psi_h
 
 # %%
-modelsol = pickle.load(open("model_ela_sol.pkl", "rb"))
+modelsol = pickle.load(open(os.getcwd()+"/" + folder_name +"model_ela_sol.pkl", "rb"))
 
 T = 48
 dt = 1
