d97593e7cadd303682e008d66a755b06500575a6
hqin-2020 <uctphqi@ucl.ac.uk>
2023-02-19 00:38:56 -0600

    1

--- SolvedModels/run_preload_quarter.sh
+++ SolvedModels/run_preload_quarter.sh
@@ -46,7 +46,7 @@ do
 #SBATCH --time=0-24:00:00
 #SBATCH --partition=caslake
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=14
+#SBATCH --cpus-per-task=3
 #SBATCH --mem-per-cpu=4000
 
 module load python/anaconda-2021.05
